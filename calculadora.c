0d187441729a962a93844b1df23fd44784ac57f6
Eduardo Martino <dudumartino4@gmail.com>
2025-10-24 11:48:24 +0000

    calc

--- calculadora.c
+++ calculadora.c
@@ -19,5 +19,25 @@ int main() {
     printf("Digite um número: ");
     scanf("%lf", &num_2);
 
+    printf("Selecione a operação:\n");
+    printf("1 - Soma\n");
+    printf("2 - Subtrair\n");
+    printf(":");
+    scanf("%d", &operacao);
+
+    switch (operacao) {
+        case 1:
+            resultado = somar(num_1, num_2);
+            break;
+        case 2:
+            resultado = subtrair(num_1, num_2);
+            break;
+        default:
+            printf("Opção inválida\n");
+    }
+
+    printf("Resultado: %.2lf\n", resultado);
+
+
     return 0;
 }
